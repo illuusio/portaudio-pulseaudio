de90c4376829e70089baaf49919ab811625a63ca
sqweek <sqweek@gmail.com>
2016-03-07 08:59:25 +0800

    pulse: add extra braces

--- src/hostapi/pulseaudio/pa_hostapi_pulseaudio.c
+++ src/hostapi/pulseaudio/pa_hostapi_pulseaudio.c
@@ -948,7 +948,9 @@ static PaError OpenStream(
         result = PulseAudioConvertPortaudioFormatToPulseAudio(hostInputSampleFormat,
                                                               &stream->inSampleSpec);
         if (result != paNoError)
+        {
             goto error;
+        }
 
         stream->inSampleSpec.rate = sampleRate;
         stream->inSampleSpec.channels = inputChannelCount;
@@ -989,7 +991,9 @@ static PaError OpenStream(
             result = PulseAudioBlockingInitRingBuffer(stream, &stream->inputRing,
                                                       stream->inputFrameSize);
             if (result != paNoError)
+            {
                 goto error;
+            }
         }
 
     }
@@ -1041,7 +1045,9 @@ static PaError OpenStream(
         result = PulseAudioConvertPortaudioFormatToPulseAudio(hostOutputSampleFormat,
                                                               &stream->outSampleSpec);
         if (result != paNoError)
+        {
             goto error;
+        }
 
         stream->outSampleSpec.rate = sampleRate;
         stream->outSampleSpec.channels = outputChannelCount;
@@ -1083,7 +1089,9 @@ static PaError OpenStream(
             result = PulseAudioBlockingInitRingBuffer(stream, &stream->outputRing,
                                                       stream->outputFrameSize);
             if (result != paNoError)
+            {
                 goto error;
+            }
         }
 
         stream->device = outputParameters->device;
